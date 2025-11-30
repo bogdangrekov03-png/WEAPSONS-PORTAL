2e951c234d0fbec833122cc018f6d9ef9a432c10
Богдан <bogdangrekov03@gmail.com>
2025-12-01 01:26:11 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
+
 
 export const metadata = {
   title: "Збройний портал",
@@ -8,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="uk">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />   {/* <-- ДОДАНО */}
+      </body>
     </html>
   );
 }
