8fe2d00241f2e254ab602930f143d6703bc23c83
bogdangrekov03-png <bogdangrekov03@gmail.com>
2025-12-01 00:47:06 +0200

    Create layout.js

---
+++ app/app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Збройний портал',
+  description: 'Інформаційний тактичний портал',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="uk">
+      <body>{children}</body>
+    </html>
+  );
+}
