f22e40b6fbde528bb918cfabb9bbc1c067bf6f07
bogdangrekov03-png <bogdangrekov03@gmail.com>
2025-12-01 00:39:58 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,12 @@
+"use client";
+import HudNav from "../components/HudNav";
+
+export default function Home() {
+  return (
+    <>
+      <HudNav />
+      <h1>Засоби ураження</h1>
+      <p>Головна сторінка Next.js</p>
+    </>
+  );
+}
